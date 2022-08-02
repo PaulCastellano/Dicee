0e42d3fa53990fea14338391438fae41eb0806e8
egaraz <egaraz@netinfo.md>
2022-08-02 15:11:15 +0300

    create connection and logic for the game

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
+fileprivate let diceImageNameList = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewL: UIImageView!
+    @IBOutlet weak var diceImageViewR: UIImageView!
+
+    var leftDiceSelection   : Int = 0
+    var rightDiceSelection  : Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPress(_ sender: Any) {
+        leftDiceSelection = Int.random(in: 0..<diceImageNameList.count)
+        rightDiceSelection = Int.random(in: 0..<diceImageNameList.count)
+        
+        diceImageViewL.image = UIImage(named: diceImageNameList[leftDiceSelection])
+        diceImageViewR.image = UIImage(named: diceImageNameList[rightDiceSelection])
+    }
+    
 }
 
